9a706ac8151d988b8ca55c0fad3e2aef7be6ef2f
eugene <firefly.eugene@gmail.com>
2020-03-06 16:30:07 +0200

    temp

--- src/twitter_bot_detection/pipelines/data_science/pipeline.py
+++ src/twitter_bot_detection/pipelines/data_science/pipeline.py
@@ -1,16 +1,16 @@
 from kedro.pipeline import node, Pipeline
-from twitter_bot_detection.pipelines.data_science.nodes import train_catboost, train_xgboost
+from twitter_bot_detection.pipelines.data_science.nodes import train_catboost
 
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
-            node(
-                func=train_xgboost,
-                inputs="user_features",
-                outputs="xgboost_model",
-                name="Training xgboost classifier",
-            ),
+            # node(
+            #     func=train_xgboost,
+            #     inputs="user_features",
+            #     outputs="xgboost_model",
+            #     name="Training xgboost classifier",
+            # ),
             node(
                 func=train_catboost,
                 inputs="user_features",
